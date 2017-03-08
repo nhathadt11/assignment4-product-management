dbcc894cce5abc2ea88f4215517120f136590178
Ha Dinh Truong Nhat <nhathadt11@gmail.com>
2017-03-08 19:32:22 +0700

    separated client app from middle layer

--- ConsoleClientApp/Program.cs
+++ ConsoleClientApp/Program.cs
@@ -1,11 +1,7 @@
 ﻿using ASM4_ProductManagement;
-using ASM4_ProductManagement.Repository;
 using ASM4_ProductManagement.Service;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleClientApp
 {
